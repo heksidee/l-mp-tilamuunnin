262e6169f56a1b942d1a27ca0adc39f09aa48dec
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-02-20 18:50:03 +0200

    valmis

--- main.js
+++ main.js
@@ -1,39 +1,64 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("lomake");
-    const select = document.getElementById("valikko");
-    const input = document.getElementById("tempe");
-    const radios = document.getElementById("desimaali");
-    const button = document.getElementById("nappi");
-});
+let painike = document.getElementById("nappi");
+painike.addEventListener("click", lämpötilaMuunnin)
 
-button.addEventListener("click", function() {
-    const lämpötila = parseFloat(input.value);
-    const muunninTyyppi = select.value;
-    let muunnettuLämpötila;
+function lämpötilaMuunnin(event) {
+    document.getElementById("nollap").innerHTML = ""
 
-    if (isNaN(lämpötila)) {
-        alert("Syötä luku.");
+    let lämpötila = parseFloat(document.getElementById("tempe").value);
+    if (lämpötila.length < 1) { 
+        alert("Pitää antaa luku!");
+        return
     }
-    switch (muunninTyyppi) {
+    if (isNaN(lämpötila)) {
+        alert("Pitää antaa luku!")
+        return
+    }
+    let valitsin = document.getElementById("valikko").value;
+
+    switch (valitsin) {
         case "fahrCels":
-            muunnettuLämpötila = (lämpötila - 32) / 1.8;
+            muunnos = (lämpötila - 32) / 1.8;
+            yksiköstä = "Fahrenheit";
+            yksikköön = "Celsius";
+            absNollaP = -273.15;
             break;
         case "celsFahr":
-            muunnettuLämpötila = (lämpötila * 1.8) + 32;
+            muunnos = (lämpötila * 1.8) + 32;
+            yksiköstä = "Celsius";
+            yksikköön = "Fahrenheit";
+            absNollaP = (-273.15 * 1.8) + 32;
             break;
         case "fahrKelv":
-            muunnettuLämpötila = (lämpötila + 459.67) / 1.8;
+            muunnos = (lämpötila + 459.67) / 1.8;
+            yksiköstä = "Fahrenheit";
+            yksikköön = "Kelvin";
+            absNollaP = -273.15 + 273.15
             break;
         case "kelvFahr":
-            muunnettuLämpötila = lämpötila * 1.8 - 459.67;
+            muunnos = lämpötila * 1.8 - 459.67;
+            yksiköstä = "Kelvin";
+            yksikköön = "Fahrenheit";
+            absNollaP = (-273.15 * 1.8) + 32;
             break;
         case "kelvCels":
-            muunnettuLämpötila = lämpötila - 273.15;
+            muunnos = lämpötila - 273.15;
+            yksiköstä = "Kelvin";
+            yksikköön = "Celsius";
+            absNollaP = -273.15
             break;
         case "celsKelv":
-            muunnettuLämpötila = lämpötila + 273.15;
+            muunnos = lämpötila + 273.15;
+            yksiköstä = "Celsius";
+            yksikköön = "Kelvin";
+            absNollaP = -273.15 + 273.15
             break;
-        
-
     }
-})
+    if (muunnos < absNollaP) {
+        document.getElementById("nollap").innerHTML = "Syöttämäsi lämpötila on alle absoluuttisen nollapisteen joka on -273,15 celsius -astetta."
+    }
+    let pyöristysValitsin = document.querySelector('input[name="desimaali"]:checked').value;
+    muunnos = muunnos.toFixed(pyöristysValitsin);
+
+    document.getElementById("tulos").innerHTML = `${lämpötila} ${yksiköstä} -astetta on ${muunnos} ${yksikköön} -astetta.`;
+    
+    }
